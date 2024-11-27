c9e655dac68b417fb37ee9d7ffd242a0331a5199
Vinisangy <vinisangy@gmail.com>
2024-11-27 00:48:09 -0300

    manipulando state do card melhor

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import Botao from '../Botao';
 import style from './Formulario.module.scss'
+import { Itarefa } from '../../types/tarefas';
 
 
-class Formulario extends React.Component {
-    state= {
+class Formulario extends React.Component<{
+    setTarefas: React.Dispatch<React.SetStateAction<Itarefa[]>>
+}> {
+    state = {
         tarefa: "",
         tempo: "00:00",
     }
 
     adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
         evento.preventDefault();
-        console.log('state: ', this.state)
+        this.props.setTarefas(tarefasAntigas =>
+            [
+                ...tarefasAntigas,
+                {
+                    ...this.state,
+                    selecionado: false,
+                    completado: false
+                }
+            ]
+        )
+        this.setState({
+            tarefa: "",
+            tempo: "00:00"
+        })
     }
 
     render() {
@@ -25,7 +41,7 @@ class Formulario extends React.Component {
                         type='text'
                         name='tarefa'
                         value={this.state.tarefa}
-                        onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
+                        onChange={evento => this.setState({ ...this.state, tarefa: evento.target.value })}
                         id='tarefa'
                         placeholder='o que voce quer estudar?'
                         required
@@ -42,14 +58,14 @@ class Formulario extends React.Component {
                         step='1'
                         name='tempo'
                         value={this.state.tempo}
-                        onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
+                        onChange={evento => this.setState({ ...this.state, tempo: evento.target.value })}
                         id='tempo'
                         min="00:00:00"
                         max="01:30:00"
                         required
                     />
                 </div>
-                <Botao>
+                <Botao type="submit">
                     Adicionar
                 </Botao>
             </form>
