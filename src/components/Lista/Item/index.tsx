d3a0ce6eea45921619d3829e5e0507fc7be704ff
Vinisangy <vinisangy@gmail.com>
2024-11-28 01:06:59 -0300

    completando tarefa apos tempo 0

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -9,8 +9,8 @@ interface Props extends Itarefa {
 export default function Item({ tarefa, tempo, selecionado, completado, id, selecionaTarefa }: Props) {
     return (
         <li
-            className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
-            onClick={() => selecionaTarefa(
+            className={`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado : " "}`}
+            onClick={() => !completado && selecionaTarefa(
                 {
                     tarefa,
                     tempo,
@@ -22,6 +22,7 @@ export default function Item({ tarefa, tempo, selecionado, completado, id, selec
         >
             <h3> {tarefa} </h3>
             <span> {tempo} </span>
+            {completado && <span className={style.concluido} aria-label='tarefa completada'></span>}
         </li>
     )
 }
